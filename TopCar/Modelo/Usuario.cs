f859b2ecf83f70bc509f1358675c901049417f6a
Dell <Dell@DESKTOP-5P573EN>
2022-11-09 20:45:09 -0300

    Adicionar arquivos de projeto.

---
+++ TopCar/Modelo/Usuario.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopCar.Modelo
+{
+    public class Usuario
+    {
+        // variáveis da tela Cadastro Usuário
+        public int Id { get; set; }
+
+        public String nome { get; set; }
+        
+        public String dataNasc  { get; set; }
+        public String cpf  { get; set; }
+
+        public String senhaCadastro { get; set; }
+
+        public String email { get; set; }
+
+
+        //variáveis da tela Mudar senha
+
+        public String mudarSenha { get; set; }
+
+
+        // Variável da tela Relatar problema
+        public  String relatarProblema { get; set; }
+
+     
+        //Variáveis da página Simular seguro 
+        public String marcaCarro { get; set; }
+
+        public String anoDeFabricacao { get; set; }
+
+        public String anoModelo { get; set; }
+
+        public String modeloCarro { get; set; }
+
+        public String placaCarro { get; set; }
+
+
+        //Variáveis da página Sim.Seg 1
+        public String nomeSegurado { get; set; }
+
+        public String emailSegurado { get; set; }
+
+        public String celularCegurado { get; set; }
+
+        public String dataNascimento { get; set; }
+
+        // Variáveis da página Sim.Seg 2
+        public String km { get; set; }
+    }
+}
---
+++ TopCar/Modelo/Validacao.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopCar.Modelo
+{
+    public class Validacao
+    {
+        public String mensagem { get; set; }
+
+
+        public void validarUsuario(List<String>ListaUsuarios)
+        {
+            this.mensagem = "";
+
+            if (ListaUsuarios[1].Length < 5)
+                this.mensagem += "Nome deve ter mais que 5 caracteres \n";
+            if (ListaUsuarios[1].Length > 20)
+                this.mensagem += "Nome deve ter menos que 20 caracteres \n";
+
+
+            if (ListaUsuarios[2].Length < 7)
+                this.mensagem += "Data de nascimento deve ter mais que 7 caracteres \n";
+            if (ListaUsuarios[2].Length > 9)
+                this.mensagem += "Data de nascimento deve ter menos que 9 caracteres \n";
+
+
+            if (ListaUsuarios[3].Length < 10)
+                this.mensagem += "CPF deve ter mais que 10 caracteres \n";
+            if (ListaUsuarios[3].Length > 12)
+                this.mensagem += "CPF deve ter menos que 12 caracteres \n";
+
+
+            if (ListaUsuarios[4].Length < 4)
+                this.mensagem += "Senha para cadastro deve ter mais que 4 caracteres \n";
+            if (ListaUsuarios[4].Length > 6)
+                this.mensagem += "Senha para cadastro deve ter menos que 6 caracteres \n";
+
+            if (ListaUsuarios[5].Length < 5)
+                this.mensagem += "email deve ter mais que 3 caracteres \n";
+            if (ListaUsuarios[5].Length > 10)
+                this.mensagem += "email deve ter menos que 10 caracteres \n";
+
+        }
+    }
+}
